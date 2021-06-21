53336cdd6aa7f77f0e26fe6c936f8fecd5b49467
Makihara <clmakihara@gmail.com>
2021-06-21 11:00:16 -0300

    Initial commit.

---
+++ src/main/java/com/dio/crud/sala/sala/SalaApplication.java
@@ -0,0 +1,13 @@
+package com.dio.crud.sala.sala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaApplication.class, args);
+	}
+
+}
